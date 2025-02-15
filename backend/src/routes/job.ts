51971f1c26e4c2af4eaa93a35743625ec2c87f30
Sayan Banerjee <sayanbanerjee2002@gmail.com>
2025-02-15 12:56:59 +0530

    jobController converted to ts
    Signed-off-by: Sayan Banerjee <sayanbanerjee2002@gmail.com>

--- backend/src/routes/job.ts
+++ backend/src/routes/job.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllJobs, getJobByName, getJobYamlByName, getJobs } from "../controllers/job.js";
+import { getAllJobs, getJobByName, getJobYamlByName, getJobs } from "../controllers/job";
 
 const router = express.Router();
 
