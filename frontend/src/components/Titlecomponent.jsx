eae0da98c993320174ce75606c1807bbf4b43cd8
Shrutim1505 <shrutimurthy2103@gmail.com>
2025-04-18 10:44:55 +0000

    resolved
    Signed-off-by: Shrutim1505 <shrutimurthy2103@gmail.com>
    
    format check
    
    Signed-off-by: Shrutim1505 <shrutimurthy2103@gmail.com>

--- frontend/src/components/Titlecomponent.jsx
+++ frontend/src/components/Titlecomponent.jsx
@@ -5,20 +5,18 @@ const TitleComponent = ({ text }) => {
         <Typography
             variant="h4"
             align="center"
-            className="fw-bold d-block text-center w-100 py-3 mb-4 border-bottom border-top rounded mx-auto"
+            className="text-center rounded mx-auto"
             sx={{
                 color: "#dc3545",
                 letterSpacing: "1px",
                 textTransform: "uppercase",
                 maxWidth: "80%",
-                boxShadow: "0 4px 6px rgba(220, 53, 69, 0.1)",
                 position: "relative",
                 "&::after": {
                     content: "''",
                     position: "absolute",
                     width: "30%",
                     height: "3px",
-                    backgroundColor: "#dc3545",
                     bottom: "-3px",
                     left: "35%",
                 },
